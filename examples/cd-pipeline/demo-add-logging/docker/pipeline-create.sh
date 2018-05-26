ae1e4d47c8c9c639c1fae119a765bac091356327
Manuel Pais <me@manuelpais.net>
2018-05-26 10:12:53 +0200

    fix jenkins log issue

--- examples/cd-pipeline/demo-add-logging/docker/pipeline-create.sh
+++ examples/cd-pipeline/demo-add-logging/docker/pipeline-create.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# make sure Jenkins log file exists before docker-compose to avoid it gets created as a 
+# directory when mounted on a container
+mkdir -p /volumes/jenkins/
+touch /volumes/jenkins/jenkins.log
+
 docker-compose down -v
+docker-compose rm -v filebeat
 docker-compose build
 docker-compose up -d
 
