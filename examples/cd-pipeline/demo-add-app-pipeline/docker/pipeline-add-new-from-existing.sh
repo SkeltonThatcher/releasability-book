9955e08d2e400017539b50d6478d4e6b6c9f70fb
Manuel Pais <manuelpais@gmail.com>
2019-06-11 13:09:42 +0200

    Update pipeline-add-new-from-existing.sh

--- examples/cd-pipeline/demo-add-app-pipeline/docker/pipeline-add-new-from-existing.sh
+++ examples/cd-pipeline/demo-add-app-pipeline/docker/pipeline-add-new-from-existing.sh
@@ -12,9 +12,9 @@ then
 	exit -1
 fi
 
-USERNAME=$(basename $(dirname $NEW_REPO))
+REPO_OWNER=$(basename $(dirname $NEW_REPO))
 
-if [[ ${USERNAME} -eq "username" ]]
+if [[ ${REPO_OWNER} = "username" ]]
 then
         echo "Please replace 'username' with the actual repo owner username in the URL:"
 	echo "${NEW_REPO}"
