14e4e2df328b76a478285ccbde4a1bd88e7622e6
Manuel Pais <manuel.pais@gmail.com>
2019-06-12 15:08:56 +0200

    update demo

--- examples/cd-pipeline/demo-monitoring-cicd-resources/docker/pipeline-create.sh
+++ examples/cd-pipeline/demo-monitoring-cicd-resources/docker/pipeline-create.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# make sure Jenkins log file exists before docker-compose to avoid it gets created as a 
-# directory when mounted on a container
-mkdir -p /volumes/jenkins/
-touch /volumes/jenkins/jenkins.log
-
 docker-compose down -v
-docker-compose rm -v filebeat
 docker-compose build
 docker-compose up -d
-
-docker logs -f -t jenkins &> /volumes/jenkins/jenkins.log &
